6407fd76a469099d9a30928e5af90a2524a25434
miku <svartkatt39@gmail.com>
2022-12-05 22:29:10 +0100

    make base view

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-print("hello world"); 
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        result = request.form
+        #データを投稿したらデータベースを更新、データベースからアイテムを取得して表示する
+        return render_template("index.html", result = result)
+    
+    return render_template('index.html', 
+        #データベースからアイテムを取得して表示する
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
