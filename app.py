910ad7c9c615b28923da5ed86addcffa5fbb1a12
miku0129 <svartkatt39@gmail.com>
2022-12-06 20:55:56 +0100

    modefied docker-compose.yml

--- app.py
+++ app.py
@@ -35,14 +35,6 @@ def index():
     todos = db.session.execute(db.select(Todo)).all()
     return render_template('index.html', todos=todos)
 
-# print("hello world")
-# @app.route('/')
-# def index():
-#     return render_template('test.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=8080)
-    # port = int(os.environ.get('PORT', 8080))
-    # app.run(debug=True, host='0.0.0.0', port=port)
