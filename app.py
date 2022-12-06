d3e388281b6a8941fd9643b4ea3e58f1117fbfb7
miku <svartkatt39@gmail.com>
2022-12-06 16:30:10 +0100

    modefy sampleTodo

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
+load_dotenv()
+
+print(os.getenv("DATABASE_URL"))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+db = SQLAlchemy(app) 
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo = db.Column(db.String(128), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route('/', methods = ['POST', 'GET'])
 def index():
+    #post時にデータベースを更新、データベースからアイテムを取得して表示する
     if request.method == 'POST':
-        result = request.form
-        #データを投稿したらデータベースを更新、データベースからアイテムを取得して表示する
-        return render_template("index.html", result = result)
+        todo = Todo(
+            todo=request.form["todo"]
+        )
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for("index"))
     
-    #データベースからアイテムを取得して表示する
-    return render_template('index.html', 
-    )
+    #get時はデータベースからアイテムを取得して表示する
+    todos = db.session.execute(db.select(Todo)).all()
+    return render_template('index.html', todos=todos)
 
 
 if __name__ == '__main__':
